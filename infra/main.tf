148ae9f354e8a2d170a52281c25cbac131414d9c
kauanvarella <kauanvarella@gmail.com>
2022-07-28 04:13:19 -0300

    ...

---
+++ infra/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+resource "aws_instance" "app_server" {
+    ami = var.ami
+    instance_type = var.instancia
+    key_name = var.chave
+    tags = {
+        Name = var.tagname
+    }
+    associate_public_ip_address = true
+}
+
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.app_server.id
+  allocation_id = var.pub_ip
+}
+
+resource "aws_ami_from_instance" "AMI_Prod" {
+  name = "imagem-producao"
+  source_instance_id = aws_instance.app_server.id
+  snapshot_without_reboot = true
+  timeouts {
+    create = "5m"
+  }
+  depends_on = [
+    aws_instance.app_server
+  ]
+} 
---
+++ infra/variables.tf
@@ -0,0 +1,27 @@
+variable "regiao_aws" {
+  type = string
+}
+
+variable "chave" {
+  type = string
+}
+
+variable "instancia" {
+  type = string
+}
+
+variable "gruposeguranca" {
+  type = string
+}
+
+variable "tagname" {
+  type = string
+}
+
+variable "ami" {
+  type = string
+}
+
+variable "pub_ip" {
+  type = string
+}
