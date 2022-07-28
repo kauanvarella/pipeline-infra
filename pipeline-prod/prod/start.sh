4bb968371de6294797fd6a4e18496aafaf4857d0
kauanvarella <kauanvarella@gmail.com>
2022-07-28 06:49:28 -0300

    ...

--- pipeline-prod/prod/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo docker start app
-sudo docker start db
-sudo docker start influxdb
--- prod/app-prod.tf
+++ prod/app-prod.tf
@@ -1,12 +1,12 @@
-module "aws-homolog" {
-  source = "../infra-homolog"
+module "aws-prod" {
+  source = "../infra"
   instancia = "t2.micro"
   regiao_aws = "us-west-2"
   chave = "ssh-prod-meuapp"
-  gruposeguranca = "acesso_homolog"
-  tagname = "app-homolog"
+  gruposeguranca = "acesso_prod"
+  tagname = "app-prod"
   ami = "ami-0d70546e43a941d70"
-  pub_ip = "eipalloc-073ccad585620e5d0"
+  pub_ip = "eipalloc-043143858d9147b05"
 }
 
 provider "aws" {
